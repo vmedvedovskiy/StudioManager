727702fcb4e3ecd2c556975b0564dd222b69cb4e
react <vlad.medvedovskiy@gmail.com>
2019-01-04 00:08:46 +0200

    Added translations support

--- StudioManager.Public/ClientApp/src/app/app.module.ts
+++ StudioManager.Public/ClientApp/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CalendarModule as AppCalendarModule } from './calendar/calendar.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, '/assets/translations');
+}
 
 @NgModule({
     declarations: [
@@ -24,7 +30,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
             pathMatch: 'full'
         }]),
         BrowserAnimationsModule,
-        MatSidenavModule
+        MatSidenavModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
